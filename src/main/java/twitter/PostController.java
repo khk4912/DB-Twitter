21bf32cd3da4899eaa540d86b51990c654e584fb
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-11-15 23:13:06 +0900

    ✨ feat: Now userInfo's width changes dynamically

--- src/main/java/twitter/PostController.java
+++ src/main/java/twitter/PostController.java
@@ -3,6 +3,7 @@ package twitter;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
 
 public class PostController {
 
@@ -15,10 +16,20 @@ public class PostController {
     @FXML
     Text contentText;
 
+    @FXML
+    ColumnConstraints userNameGridPane;
+
+    @FXML
+    ColumnConstraints userHandleGridPane;
+
     public void initalizePost(String username, String handle, String contentText) {
+
         this.usernameLabel.setText(username);
         this.handleLabel.setText(handle);
         this.contentText.setText(contentText);
+
+        this.userNameGridPane.setPrefWidth(username.length() * 10);
+        this.userHandleGridPane.setPrefWidth(handle.length() * 10);
 
     }
 
