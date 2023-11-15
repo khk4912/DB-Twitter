a0835ad9cfbb42d61eca6787c83e150105e66bff
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-11-16 00:29:54 +0900

    ✨ feat: Add more post when scroll reached bottom

--- src/main/java/twitter/MainViewController.java
+++ src/main/java/twitter/MainViewController.java
@@ -6,10 +6,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.control.ScrollPane;
 
 public class MainViewController {
     @FXML
     private GridPane postGridPane;
+
+    @FXML
+    private ScrollPane postScrollPane;
 
     public void initialize() {
 
@@ -18,6 +22,7 @@ public class MainViewController {
 
         addPost("Demo2", "@random",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac interdum nisl. Maecenas rhoncus rhoncus libero, ac lacinia nibh elementum nec. Donec tempus, lacus id porta cursus, erat lectus condimentum tortor, sed tincidunt dui est a erat. Nunc vel tellus eget mi gravida lacinia tincidunt ut nibh. Nam vitae enim in elit consequat pretium. Suspendisse porttitor nisi quis massa egestas commodo. Integer ultricies vel massa a iaculis. Donec porta, diam ac tincidunt malesuada, ipsum eros consequat sapien, pretium pharetra mi risus vitae tellus. Aliquam varius at eros vel luctus.");
+
     }
 
     private void addPost(String username, String handle, String contentText) {
@@ -34,6 +39,17 @@ public class MainViewController {
         } catch (IOException e) {
             System.out.println("Error loading post.fxml");
             System.out.println(e.getMessage());
+        }
+
+    }
+
+    @FXML
+    private void scrollFinished() {
+
+        double scrollHeight = postScrollPane.getHeight();
+
+        if (scrollHeight > 0.95) {
+            // TODO: Add more posts!
         }
 
     }
