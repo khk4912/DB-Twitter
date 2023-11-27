ad46c3c8131a5e948bb45420da2f2a12dfd09ae5
tjfdlvTkr <junha212@gmail.com>
2023-11-27 21:10:41 +0900

    feat: Implement Follow and Following and Show Follow list

---
+++ src/main/java/twitter/ShowFollowing.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class ShowFollowing {
+    private Connection con;
+
+    public ShowFollowing(Connection con) {
+        this.con = con;
+    }
+
+    public void execute(String userID) {
+        try {
+            String query = "SELECT following_id FROM following WHERE follow_id = ?";
+            PreparedStatement pstmt = con.prepareStatement(query);
+            pstmt.setString(1, userID);
+            ResultSet rs = pstmt.executeQuery();
+
+            System.out.print("People who are following " + userID + ":");
+            while (rs.next()) {
+                System.out.print(rs.getString("following_id "));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
