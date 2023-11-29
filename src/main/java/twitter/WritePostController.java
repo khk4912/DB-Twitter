b6fe31b5d656b0e867d43c31c37b6a5e8530ffbb
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-11-29 18:29:36 +0900

    ✨ feat: Implement write post feature

--- src/main/java/twitter/WritePostController.java
+++ src/main/java/twitter/WritePostController.java
@@ -1,5 +1,32 @@
 package twitter;
 
-public class WritePostController {
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
+import twitter.db.UploadPost;
+import twitter.utils.TwitterAlert;
+
+public class WritePostController {
+    UploadPost uploadPost = new UploadPost(App.DB.getConnection(), App.loginContext.user.userID);
+
+    @FXML
+    TextArea contentTextArea;
+
+    private void uploadPost(String userID, String content) {
+        uploadPost.uploadPost(userID, content);
+    }
+
+    @FXML
+    private void upload() {
+        String content = contentTextArea.getText();
+
+        if (content.length() == 0 || content == null) {
+            TwitterAlert.info("트윗 내용이 없음", "트윗 내용을 입력해주세요.");
+            return;
+        }
+
+        uploadPost(App.loginContext.user.userID, content);
+        TwitterAlert.info("트윗 업로드 완료", "트윗이 업로드되었습니다.");
+
+    }
 }
--- src/main/java/twitter/db/UploadPost.java
+++ src/main/java/twitter/db/UploadPost.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import twitter.utils.TwitterAlert;
+
 public class UploadPost {
     private Connection con;
     private PreparedStatement pstm;
@@ -13,6 +15,23 @@ public class UploadPost {
     public UploadPost(Connection con, String user_id) {
         this.con = con;
         this.user_id = user_id;
+    }
+
+    // TODO: Image?
+    public void uploadPost(String userID, String content) {
+        String insertQuery = "insert into posts( user_id_writer, content) values (?, ?)";
+        try {
+            pstm = con.prepareStatement(insertQuery);
+            pstm.setString(1, user_id);
+            pstm.setString(2, content);
+
+            pstm.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            TwitterAlert.error("게시글 업로드 오류!", e.getMessage());
+        }
+
     }
 
     public void uploadPost() {
