89458f875cbfc8fabf7baa9c87d3d66913077fa2
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-12-03 15:27:24 +0900

    ✨ feat: Implement followlist's controller

---
+++ src/main/java/twitter/FollowListController.java
@@ -0,0 +1,40 @@
+package twitter;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
+
+import twitter.db.Follow;
+
+public class FollowListController {
+
+    @FXML
+    Text sectionName;
+
+    @FXML
+    GridPane nameGridPane;
+
+    Follow follow = new Follow(App.DB.getConnection());
+
+    public void init(boolean isFollower, String ID) {
+        if (isFollower) {
+            sectionName.setText("팔로워");
+        } else {
+            sectionName.setText("팔로잉");
+        }
+
+        ArrayList<String> list = isFollower ? follow.getFollowListOf(ID) : follow.getFollowingListOf(ID);
+
+        if (list.size() == 0) {
+            return;
+        }
+
+        for (String x : list) {
+            Label label = new Label(x);
+            nameGridPane.add(label, 0, nameGridPane.getRowCount());
+        }
+    }
+}
